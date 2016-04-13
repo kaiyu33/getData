f8477440d038353d62a723d29384fa312ff07d50
kaiyu33 <a101062225@gmail.com>
2016-04-14 01:11:12 +0800

    R0414

--- R/JAVA_getTwseData3_running.R
+++ R/JAVA_getTwseData3_running.R
@@ -12,22 +12,29 @@ csvv<-gsub(",",replacement="",csvv)
 csvv[1]
 csvv<-strsplit(csvv,";")
 csvv[[1]][16]
-for (i in 1:length(csvv)) {
-  for (j in 1:length(csvv[[1]])) {
-    ifelse(grepl("[ ]*",csvv[[i]][j]),csvv[[i]][j]<-"0",)####NNNNNNNNNNNNNNNNOOOOOOOOOOOOOOOOOO
-  }
-}
+
 
 csvv[1]
 # class(csvv)
 # length(csvv)
-csvv_dataframe<-t(as.data.frame(csvv))
+csvv_dataframe<-as.data.frame(csvv)
+csvv_dataframe<-t(as.matrix(csvv_dataframe))
 # class(csvv)
 # csvv2[,1]
 # colnames(csvv)
 # ncol(csvv)
 rownames(csvv_dataframe)<-c(2:(length(csvv)+1))
 colnames(csvv_dataframe)<-c(1:24)
+
+for (i in 1:length(csvv)) {
+  for (j in 1:length(csvv[[1]])) {
+    ifelse(grepl("\s",csvv_dataframe[i,j]),csvv_dataframe[i,j]<-"0",)####NNNNNNNNNNNNNNNNOOOOOOOOOOOOOOOOOO<U+00A0>
+  }
+}
+grepl("[ ]*"," ")
+
+# is.integer(csvv_dataframe[1,3])
+# is.name(csvv_dataframe[1,3])
 
 #還是分開寫 尤其是中文  否則Error: VECTOR_ELT() can only be applied to a 'list', not a 'raw'
 cname<-c("num","公司代號","公司名稱","股利所屬年度","權利分派基準日",
@@ -44,9 +51,24 @@ csvv_dataframe<-rbind(cname,csvv_dataframe)
 View(csvv_dataframe)
 head(csvv_dataframe)
 
-new_path00<-paste0(Upath,"JAVA_getTwseData3/",k,"YearCoupon.csv")
-write.csv(A,file = new_path00)
+new_path00<-paste0(Upath,"EXdata/JAVA_getTwseData3/",k,"YearCouponCCC.csv")
+write.csv(csvv_dataframe,file = new_path00)
 #colnames(A)<-c("ID","name","C_Year","C_BaseDate","C_Stock_Earnings","C_Stock_Other","C_Stock_ExchangeDate",""
 #""
 }
 
+A<-read.csv(new_path00)
+
+for (j in 1:length(A)) {
+  for (i in 1:length(A[[1]])) {
+    %A[%i,%j]<-gsub("<U+00A0>",replacement="0",A[i,j])
+    # ifelse(A[i,j]=="<U+00A0>",A[i,j]<-"0",break)
+    #   xx<-paste0("A[",i,",",j,"]")
+    #   assign(xx,"0")
+    #   
+    # }
+    # gsub("<U+00A0>",replacement="",A[i,j])
+    # n<- paste("x",i,sep=".")
+    
+  }
+}
