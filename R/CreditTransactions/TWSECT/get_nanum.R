72d38ddde25311846dde0a3fc0c8210ba6aa07d4
kaiyu33 <a101062225@gmail.com>
2016-04-14 18:49:53 +0800

    R

--- R/CreditTransactions/TWSECT/get_nanum.R
+++ R/CreditTransactions/TWSECT/get_nanum.R
@@ -28,14 +28,25 @@ DIRCT<-dir(getStockNumCT_path,"^[']")
 getStockNumFI_path<-paste0(Upath,"EXdata/FinancialInformation/FIData/getStockNumFI/")
 DIRFI<-dir(getStockNumFI_path,"^[0-9]")
 
-
+bodyNARow_num<-0
+bodyNA<-NULL
 for (i in 1:NROW(DIRFI)) {
-  if(substr(DIRFI[i],1,4)%in%substr(DIRCT,2,5)){}
+  if(substr(DIRFI[i],1,4)%in%substr(DIRCT,2,5)){
+    bodyNARow_num<-bodyNARow_num+1
+    bodyNA<-rbind(bodyNA,substr(DIRFI[i],1,4))
+  }
   
 }
-                               
+
 NROW(DIRFI)
 NCOL(DIRFI)
+
+bodyNARow_num<-bodyNARow_num+1
+bodyNARow<-cbind(bodyNARow_num,getStockNum)
+bodyNA<-rbind(bodyNA,bodyNARow)
+
+body_path_NA<-paste0(Upath,"EXdata/CreditTransactions/CTData/getStockNumCT/total.csv")
+write.csv(bodyNA,file = body_path_NA,fileEncoding="UTF-8") 
 
 DIR<-dir(getStockNumCT_path)
 
@@ -46,18 +57,4 @@ getStockNumforCT_DIR<-dir(getStockNumforCT_path)
 for (getStockNumforCT_RowNum in 1:length(getStockNumforCT_DIR)) {#1改成58     7
   getStockNum<-paste0("'",substr(getStockNumforCT_DIR[getStockNumforCT_RowNum],1,4))
   
-  body1<-NULL
-  for (DIRnum in length(DIR):1) {#settinggggggggggggggggggggggggggggggggggggggggggggggggggggg
-    
-    getStockNumCT_path2<-paste0(Upath,"EXdata/CreditTransactions/",DIR[DIRnum])
-    #read file
-    getStockNumCT<-read.csv(getStockNumCT_path2,fileEncoding="UTF-8",stringsAsFactors = FALSE)#<-UTF-8
-    
-    
-    
-    #write.csv file location 
-    body_path1<-paste0(Upath,"EXdata/CreditTransactions/CTData/getStockNumCT/",getStockNum,body1[1,3],".csv")
-    
-    #write.csv file
-    write.csv(body1,file = body_path1,fileEncoding="UTF-8") 
     
